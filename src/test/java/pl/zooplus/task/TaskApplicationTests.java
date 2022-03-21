9c7fd3213c53d90c8a50532adb0638934834dd2e
Tingred <exiles1@wp.pl>
2022-03-21 13:51:57 +0100

    commit

---
+++ src/test/java/pl/zooplus/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zooplus.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
